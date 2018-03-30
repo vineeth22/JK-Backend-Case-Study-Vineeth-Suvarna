722589aa499f331664e1581ae1af7b4cb2a4290f
Vineeth Suvarna <vineeth22@live.in>
2018-03-30 12:02:19 +0530

    Created path /users

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,45 @@
 const express = require('express');
 const router = express.Router();
+const https = require('https');
 
-router.get('/', (req, res)=> {
+let url = 'https://shielded-headland-24739.herokuapp.com/static/users.txt';
+
+const fetchData = (url, func) => {
+  https.get(url, res => {
+    res.setEncoding('utf-8');
+    let body = '';
+    res.on('data', data => {
+      body += data;
+    });
+    res.on('end', () => {
+      func(body);
+    });
+  });
+};
+
+const parseData = (data, func) => {
+  let users = [];
+  data.split('|').forEach(element => {
+    let user = {};
+    element.split('&').forEach(elem => {
+      let ele = elem.split('=');
+      user[ele[0]] = ele[1];
+    });
+    users.push(user);
+  });
+  func(users);
+};
+
+router.get('/', (req, res) => {
   res.send('Test');
+});
+
+router.get('/users', (req, res) => {
+  fetchData(url, (data) => {
+    parseData(data, (users)=>{
+      res.send(users);
+    });
+  });
 });
 
 module.exports = router;
